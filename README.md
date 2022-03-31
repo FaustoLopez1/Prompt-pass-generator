15e2d890f264f0cf7b17306646f26deecf810a29
Fausto Lopez <lopezfausto123@yahoo.com>
2022-03-31 16:04:50 -0700

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Prompt-pass-generator
+
+<head>
+<h1>
+    <meta charset="UTF-8" />
+
+***Prompt Password Generator READ.md*** 
+</h1>
+</head>
+
+![alt text](https://img.shields.io/badge/Description-green)
+
+
+This application is meant to help randomly generate a password that meets certain criteria by giving options to either add upper or lower case lettering and number or symbols.
+
+SO that it can create a strong password that provides greater security if those who need it.
+
+
+![alt text](https://img.shields.io/badge/Usage-green)
+
+In order to apply this application, you can either copy the code and run it ov your software or just open the link and check it out.
+
+All you need to do is follow the prompt by clicking okay or cancel.
+
+
+<a href="LINK_TO_REPO">
+<img src=https://media3.giphy.com/media/GBSlmT1p17LUaVlgbY/giphy.gif?cid=790b7611d95ecc4356ef74bbc2cbc60478ee20d6fc1d9cc5&rid=giphy.gif&ct=g alt="logo">
+</a>
+
+
+![alt text](https://img.shields.io/badge/Credits-green)
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+- https://choosealicense.com/licenses/mit/#
+- https://stackoverflow.com/questions/14494747/how-to-add-images-to-readme-md-on-github
+- https://www.w3schools.com/css/css_background.asp
+
+
+![alt text](https://img.shields.io/badge/License-green)
+
+Copyright (c) [2020] [Faustoinc.] 
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-var enter;
+//Add variables
+var check;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
@@ -9,18 +11,19 @@ var confirmLowercase;
 
 // Start Password variable values: 
 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+character = ["!", "#", "<","=", "'", "+", ",", "-", ".", "/", "(", ")", "*", "\:",">", "$", "%", "&", "\;", "?", "@", "[","_", "`", "{", "|", "]", "^",  "}", "~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Space is for the Uppercase conversion
 space = [];
-
+//  
 var choices;
 // converts letters to uppercase 
 var toUpper = function (x) {
     return x.toUpperCase();
 };
 // creates a variable for uppercase conversion
+
+
 alpha2 = alpha.map(toUpper);
 
 var get = document.querySelector("#generate");
@@ -28,7 +31,127 @@ var get = document.querySelector("#generate");
 
 // Add event listener to generate button
 
+
 get.addEventListener("click", function () {
-    ps = generatePassword();
-    document.getElementById("password").placeholder = ps;
+    pwd = generatePassword();
+    document.getElementById("password").placeholder = pwd;
 });
+
+//function to generate password
+
+function generatePassword() {
+    // Asks for user input
+
+    check = parseInt(prompt("How many characters would you like your password to be? Choose between 8 and 128"));
+
+
+    //user validation 
+
+
+    if (!check) {
+        alert("Must pick");
+    } else if (check < 8 || check > 128) {
+      check = parseInt(prompt("You must choose"));
+
+    } else {
+        // Continues once user input is validated
+        confirmNumber = confirm("Do you want contain numbers?");
+        confirmCharacter = confirm("Do you want special characters?");
+        confirmUppercase = confirm("Do you want uppercase letters?");
+        confirmLowercase = confirm("Do you want contain lowercase letters?");
+    };
+
+    // Else if for 4 negative options
+
+
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must choose!");
+
+    }
+    // if that prompts to determine choices
+
+
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+        choices = character.concat(number, alpha, alpha2);
+    }
+    // Else if for 3 options
+
+
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(alpha, alpha2);
+    }
+    // Else if for 2 options 
+
+
+    else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(number);
+
+    } else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(alpha);
+
+    } else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(alpha2);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        choices = alpha.concat(number);
+
+    } else if (confirmLowercase && confirmUppercase) {
+        choices = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(alpha2);
+    }
+    // Else if for 1 option
+
+
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLowercase) {
+        choices = alpha;
+    }
+    // Created space variable to fill uppercase conversion
+
+
+    else if (confirmUppercase) {
+        choices = space.concat(alpha2);
+    };
+
+    // password variable is an array placeholder for user generated amount of length
+    var password = [];
+
+    // Start random selection variables:
+
+
+    for (var i = 0; i < check; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+    // This joins the password array and converts it to a string
+
+
+    var pwd = password.join("");
+    UserInput(pwd);
+    return pwd;
+}
+//password into the textbox
+
+
+function UserInput(pwd) {
+    document.getElementById("password").textContent = pwd;
+
+
+}
